af6787f9e283903358518ae092a788699982d059
thewangzl <thewangzl@gmail.com>
2015-05-31 23:18:57 +0800

    已做到从数据库中读取、多页面

---
+++ code.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python
+
+import web
+
+#render = web.template.render('templates/')
+render = web.template.render('templates/')
+db = web.database(dbn='sqlite',db='MovieSite.db')
+
+urls = (
+	'/','index',
+        '/movie/(\d+)','movie'
+)
+
+class index:
+	def GET(self):
+		movies = db.select('movie')
+		return render.index(movies)
+
+class movie:
+	def GET(self,movie_id):
+#		movie_id = int(movie_id)
+#		condition = 'id=' + movie_id
+#		movie = db.select('movie',where=condition,vars=locals())[0]
+		movie = db.select('movie',where='id=$int(movie_id)',vars=locals())[0]
+		return render.movie(movie)
+
+
+if __name__ == "__main__":
+	app = web.application(urls,globals())
+	app.run()
+
+
